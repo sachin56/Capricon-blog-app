e09a339b012cee0a2cca984238ca8160d5859656
Sachin Heshan Munasinghe <heshanf45@gmail.com>
2023-10-17 13:39:13 +0530

    add logout function in controller

--- code/app/Http/Controllers/LoginController.php
+++ code/app/Http/Controllers/LoginController.php
@@ -39,4 +39,13 @@ class LoginController extends Controller
         ])->onlyInput('email');
 
     }
+
+    public function logout(Request $request)
+    {
+        Auth::logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+        return redirect()->route('login')
+            ->withSuccess('You have logged out successfully!');;
+    }
 }
