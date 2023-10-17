7b19f7aa4106512dbcda0f97bf9cec18a3100627
Sachin Heshan Munasinghe <heshanf45@gmail.com>
2023-10-17 12:45:37 +0530

    add postman collection

--- code/routes/web.php
+++ code/routes/web.php
@@ -21,11 +21,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-
-Route::get('/home',[HomeController::class ,'index']);
-
-Route::get('/dashboard',[HomeController::class ,'index'])->name('dashboard');
-
 //Login routes
 Route::get('/login',[LoginController::class ,'index'])->name('login');
 Route::post('/login',[LoginController::class ,'checklogin']);
